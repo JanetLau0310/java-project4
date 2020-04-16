b90b06ccac480b5e0e53d37afa23c40e3fa24827
URNOTJANET <janetlx0115@gmail.com>
2020-04-16 01:11:10 -0400

    Update Model.java

--- jrails/src/main/java/jrails/Model.java
+++ jrails/src/main/java/jrails/Model.java
@@ -242,7 +242,7 @@ public class Model {
     public void destroy() {
         try {
             //if not save to db before
-            if (this.unique_id == 0 || find(this.getClass(),this.id()) == null) {
+            if (find(this.getClass(),this.id()) == null) {
                 throw new Exception();
             }
             else {
